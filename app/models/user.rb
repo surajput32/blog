d5453a324d64c18e85fe91ac3ef490ec07b6ad24
surajput32 <suraj21071998@gmail.com>
2021-12-08 16:52:31 +0530

    Add class method to count the posts associated with users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,16 @@
 class User < ApplicationRecord
   has_secure_password
+
+  # associations
+  has_many :posts
+
+  def self.all_with_posts_count_having_at_least_one_post
+    joins(:posts)
+      .distinct
+      .select('users.id, COUNT(posts.id) AS number_of_posts')
+      .group('users.id')
+      .map do |user|
+        { user_id: user.id, number_of_posts: user.number_of_posts }
+      end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  # associations
+  it { should have_many(:posts) }
+
+  describe 'Class methods' do
+    describe '#all_with_posts_count_having_at_least_one_post' do
+      it 'returns user ids and post counts of all users having posts' do
+        create_list(:user, 2)
+        posts = create_list(:post, 2)
+        result = User.all_with_posts_count_having_at_least_one_post
+
+        expect(result).to match_array(
+          [
+            { user_id: posts.first.user_id, number_of_posts: 1 },
+            { user_id: posts.second.user_id, number_of_posts: 1 }
+          ]
+        )
+      end
+    end
+  end
 end
